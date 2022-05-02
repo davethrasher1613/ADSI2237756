f347a8faf819cd0239dd5f830d78116d56118dd4
DanielAlbarracin <daveangelofdeath@gmail.com>
2022-05-02 13:57:59 -0500

    funciones flecha

---
+++ descuento.js
@@ -0,0 +1,34 @@
+/*  function descuento(precio, descuento){
+ var descuento=50
+ const descuentoinicial=precio/descuento
+ var preciofinal= 100/descuentoinicial
+ console.log(`el precio ingresado es de ${precio}`)
+console.log(`el descuento es de ${descuento}`)
+ return 'el precio con descuento  es de:' + preciofinal;
+}
+console.log(descuento(200,50)) */
+
+
+
+
+
+
+const descuento=(precio,descuento)=>{
+
+
+
+
+    var descuento=50
+const descuentoinicial=precio/descuento
+var preciofinal= 100/descuentoinicial
+console.log(`el precio ingresado es de ${precio}`)
+console.log(`el descuento es de ${descuento}`)
+return 'el precio con descuento  es de:' + preciofinal;
+}
+
+
+
+
+
+console.log(descuento(200,50))
+
---
+++ primos.js
@@ -0,0 +1,47 @@
+/* function numerosprimos(numeroingresado){
+let residuo=0
+
+for (let i = 1; i <= numeroingresado ; i++) {
+    if (numeroingresado % i === 0) {
+        residuo++
+    }
+
+}
+
+if (residuo==2) {
+    console.log(`El numero ${numeroingresado} si es un numero primo`)
+    
+}else{
+    console.log(`El numero ${numeroingresado} no es un numero primo`)
+}
+}
+console.log(numerosprimos(3)) */
+
+
+//con un parametros
+
+
+
+
+const numerosprimos=numeroingresado=>{
+
+    let residuo=0
+
+    for (let i = 1; i <= numeroingresado ; i++) {
+        if (numeroingresado % i === 0) {
+            residuo++
+        }
+    
+    }
+    
+    if (residuo==2) {
+        console.log(`El numero ${numeroingresado} si es un numero primo`)
+        
+    }else{
+        console.log(`El numero ${numeroingresado} no es un numero primo`)
+    }
+    }
+    console.log(numerosprimos(4))
+
+
+
