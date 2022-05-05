e58fca665f63e5835e306d7418bc72bbb3f2232b
davethrasher1613 <daveangelofdeath@gmail.com>
2022-05-05 10:55:05 -0500

    tareas javascript sexto trimestre,funcion flecha ciclos,arreglos,objetos literales)

--- fecha.js
+++ fecha.js
@@ -2,14 +2,15 @@
 
 
 
-/* function fecha(a){
+const fecha=(a)=>{
 
 var fechaingresada= new Date(a)
-var fechadehoy=new Date()
+var fechadehoy=new Date("2022-05-02")
 console.log(fechaingresada);
 console.log(fechadehoy);
 console.log()
 
+var fechadelpc = fechadehoy.toISOString().split("T")[0]
 
 
 if (fechadehoy>fechaingresada) {
@@ -21,75 +22,13 @@ if (fechadehoy>fechaingresada) {
 }
 
 }
-fecha("2020-05-02")   */
-
-
-
-
-
-
-const fecha=a=>{
-
-    var fechaingresada= new Date(a)
-    var fechadehoy=new Date("2022-05-02")
-    console.log(fechaingresada);
-    console.log(fechadehoy);
-    console.log()
-    
-    
-    if (fechadehoy>fechaingresada) {
-        console.log("la fecha que coloco es anterior")
-    }else if(fechadehoy<fechaingresada){
-        console.log("la fecha que coloco es posterior")
-    }else{
-        console.log("la fecha que coloco es actual")
-
-    }
-}
-fecha("2022-05-02") 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fecha("2023-05-02") 
 
 
 
 
 /* 
-
-function fecha(fecha){
+const fecha=(fecha)=>{
 
     var fechaingresada= new Date(fecha)
     var fechadehoy=new Date()
@@ -101,12 +40,12 @@ function fecha(fecha){
     
     
     if (fechadelpc<fechaingresada) {
-        console.log("la fecha que coloco es posterior")
-    }else if(fechadelpc>fechaingresada){
         console.log("la fecha que coloco es anterior")
+    }else if(fechadelpc>fechaingresada){
+        console.log("la fecha que coloco es posterior")
     }else{
         console.log("la fecha que coloco es actual")
     }
     
     }
-    fecha("2021-05-02")  */
+    fecha("2020-05-02")  */
--- primos.js
+++ primos.js
@@ -1,4 +1,4 @@
-/* function numerosprimos(numeroingresado){
+const numerosprimos=(numeroingresado)=>{
 let residuo=0
 
 for (let i = 1; i <= numeroingresado ; i++) {
@@ -15,33 +15,4 @@ if (residuo==2) {
     console.log(`El numero ${numeroingresado} no es un numero primo`)
 }
 }
-console.log(numerosprimos(3)) */
-
-
-//con un parametros
-
-
-
-
-const numerosprimos=numeroingresado=>{
-
-    let residuo=0
-
-    for (let i = 1; i <= numeroingresado ; i++) {
-        if (numeroingresado % i === 0) {
-            residuo++
-        }
-    
-    }
-    
-    if (residuo==2) {
-        console.log(`El numero ${numeroingresado} si es un numero primo`)
-        
-    }else{
-        console.log(`El numero ${numeroingresado} no es un numero primo`)
-    }
-    }
-    console.log(numerosprimos(4))
-
-
-
+console.log(numerosprimos(4))
